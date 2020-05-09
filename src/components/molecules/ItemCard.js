f50a7a7763d28947425cb2f0335bd7b24484c5b9
Kris Viceral <krisviceral@gmail.com>
2020-05-09 17:09:53 -0400

    card design wip

--- src/components/molecules/ItemCard.js
+++ src/components/molecules/ItemCard.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Badge from "../atoms/Badge";
 
-const ItemCard = ({}) => {
+const ItemCard = ({name, address}) => {
+
 
     //https://images.unsplash.com/photo-1428515613728-6b4607e44363?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80
     //The `style` prop expects a mapping from style properties to values, not a string. For example, style={{marginRight: spacing + 'em'}} when using JSX.
@@ -16,23 +17,25 @@ const ItemCard = ({}) => {
                 <div className="bg-white p-6 rounded-lg shadow-lg">
                     <div className="flex items-baseline">
                         <Badge text={"small"}/>
-                        <div className="ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide">
-                            1 beds 2 baths
-                        </div>
                     </div>
-                    <h4 className="mt-1 font-semibold text-lg leading-tight truncate">FooBar</h4>
-                    <div className="mt-1">
-                        1,200
-                        <span className="text-gray-600 text-sm"> / wk</span>
+                    <h4 className="mt-1 font-semibold text-lg leading-tight truncate">{name}</h4>
+                    <div className="flex mb-4 text-gray-600 text-sm mt-1">
+                        <span>
+                            <svg className="h-4 w-4 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/>
+                            </svg>
+
+                        </span>
+                        <span className="pl-1">
+                            {address}
+                        </span>
+
                     </div>
-                    <div className="mt-2 flex items-center">
-                        <svg viewBox="0 0 24 24"
-                             className="h-4 w-4 fill-current">
-                            <path
-                                d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"/>
-                        </svg>
-                        <span className="ml-2 text-gray-600 text-sm">2 reviews</span>
-                    </div>
+                    <button type="button"
+                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
+                        View
+                    </button>
                 </div>
             </div>
         </div>
--- src/components/organism/ResultsContainer.js
+++ src/components/organism/ResultsContainer.js
@@ -4,16 +4,16 @@ import ItemCard from "../molecules/ItemCard";
 const ResultsContainer = ({}) => {
 
     const items = [
-        {id: 1, name: "Foo", bar: "Bar"},
-        {id: 2, name: "Foo", bar: "Bar"},
-        {id: 3, name: "Foo", bar: "Bar"},
-        {id: 4, name: "Foo", bar: "Bar"},
+        {id: 1, name: "FooBakery", address: "123 Bakery St, Hon"},
+        {id: 2, name: "Foo", address: "Bar"},
+        {id: 3, name: "Foo", address: "Bar"},
+        {id: 4, name: "Foo", address: "Bar"},
     ];
 
-    const itemCard = () => {
+    const itemCard = (item) => {
         return (
             <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-                <ItemCard/>
+                <ItemCard name={item.name} address={item.address}/>
             </div>
         )
     };
@@ -21,7 +21,7 @@ const ResultsContainer = ({}) => {
     return (
         <div className="container my-12 mx-auto px-4 md:px-12">
             <div className="flex flex-wrap -mx-1 lg:-mx-4">
-                {items.map(i => itemCard())}
+                {items.map(i => itemCard(i))}
 
             </div>
         </div>
