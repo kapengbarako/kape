e923a32a3a930387cc4ef3432d890ca9aa2ba740
Kris Viceral <krisviceral@gmail.com>
2020-05-09 14:51:56 -0400

    add button

--- src/components/templates/Recipes.js
+++ src/components/templates/Recipes.js
@@ -35,10 +35,10 @@ const Recipes = ({handler, text}) => {
                     </div>
                 </div>
                 <div className="w-1/5 h-12">
-                    <span className="pl-2 pt-1 inline-flex rounded-md shadow-sm">
+                    <span className="pt-1 pl-2 inline-flex rounded-md shadow-sm">
                       <button type="button"
                               className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
-                            <svg className="h6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
                             </svg>
                       </button>
