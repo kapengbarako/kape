8abdfbc9d63d48f8251969ffc1e71579f59af42c
Kris Viceral <krisviceral@gmail.com>
2020-05-09 20:40:07 -0400

    good enough

--- src/components/molecules/ItemCard.js
+++ src/components/molecules/ItemCard.js
@@ -19,9 +19,18 @@ const ItemCard = ({name, address}) => {
                         <h3 className="text-lg leading-6 font-medium text-gray-900">
                             {name}
                         </h3>
-                        <p className="mt-1 text-sm leading-5 text-gray-500">
-                            {address}
-                        </p>
+                        <a href="#"
+                           className="mt-1 group flex items-center text-sm font-sm text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-200 transition ease-in-out duration-150">
+                            <svg className="flex-shrink-0 mr-2 h-4 w-4 text-gray-400 group-focus:text-gray-500 transition ease-in-out duration-150" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/>
+                            </svg>
+                            <span className="truncate">
+                              {address}
+                            </span>
+                        </a>
                     </div>
                 </div>
                 <div className="w-2/6 pt-4">
